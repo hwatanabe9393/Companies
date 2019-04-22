4e4263d93004a3cd9cf3ce34d583c66379564d8a
Hikaru Watanabe <hikaruwatanabe@hikarus-mbp.attlocal.net>
2019-04-21 20:35:05 -0500

    Company cell created. TableView footer view added

--- Companies/Controller/Companies/CompaniesController.swift
+++ Companies/Controller/Companies/CompaniesController.swift
@@ -9,8 +9,13 @@
 import UIKit
 
 class CompaniesController: UITableViewController {
+    let companyCellId = "companyCellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(CompanyCell.self, forCellReuseIdentifier: companyCellId)
+        
         setup()
     }
     
@@ -18,7 +23,7 @@ class CompaniesController: UITableViewController {
     fileprivate func setup(){
         //TableView
         tableView.backgroundColor = UIColor(red: 9/255, green: 45/255, blue: 64/255, alpha: 1)
-        tableView.separatorStyle = .none
+        tableView.tableFooterView = UIView()
         
         //Nav set up
         navigationItem.title = "Companies"
@@ -32,5 +37,20 @@ class CompaniesController: UITableViewController {
     
     @objc func handleResetCompany(_ sender: Any){
         //TODO:- Reset company
+    }
+    
+    //MARK:- TableView
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: companyCellId, for: indexPath) as! CompanyCell
+        cell.backgroundColor = .yellow
+        return cell
     }
 }
